bdf7643cb619b8dfac6119523ff3494aa7aaf171
Rey Halsall <reyeduardohalsallquintero8@gmail.com>
2022-06-16 14:33:36 -0600

    Adding Post and Put Methods

--- WebApplication1/Controllers/LibreriaController.cs
+++ WebApplication1/Controllers/LibreriaController.cs
@@ -34,11 +34,42 @@ namespace WebApplication1.Controllers
                 return BadRequest();
             }
             Libro libro = _context.Libros.FirstOrDefault(lib => lib.Id.ToString() == id);
-            if(libro == null)
+            if (libro == null)
             {
                 return NotFound();
             }
             return Ok(libro);
+        }
+
+        [HttpPost]
+        public ActionResult<Libro> AddLibro(Libro libro)
+        {
+            if (libro == null)
+            {
+                return BadRequest();
+            }
+            _context.Libros.Add(libro);
+            return Ok(_context.SaveChanges());
+        }
+
+        [HttpPut("update")]
+        public ActionResult<Libro> UpdateLibro(Libro libro)
+        {
+            if(libro == null)
+            {
+                return BadRequest();
+            }
+
+            Libro lib = _context.Libros.Find(libro.Id);
+            if (lib == null) return BadRequest();
+
+            lib.Temas = libro.Temas;
+            lib.Editorial = libro.Editorial;
+            lib.Titulo = libro.Titulo;
+            lib.Autor = libro.Autor;
+
+            _context.Libros.Update(lib);
+            return Ok(_context.SaveChanges());
         }
     }
 }
